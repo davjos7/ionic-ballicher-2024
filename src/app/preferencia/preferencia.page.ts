eb4f8030b140de2d815c1999d48bca9f871863ef
davjos7 <josephdavilahorna@gmail.com>
2024-09-18 19:01:59 -0500

    jsonpreferencia

--- src/app/preferencia/preferencia.page.ts
+++ src/app/preferencia/preferencia.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { IonicModule, NavController} from '@ionic/angular';
 import { RouterModule,Router} from '@angular/router';
-
+import { NgFor } from '@angular/common';
+import * as preferencia from '../../assets/json/preferencias.json';
 
 
 @Component({
@@ -9,12 +10,13 @@ import { RouterModule,Router} from '@angular/router';
   templateUrl: './preferencia.page.html',
   styleUrls: ['./preferencia.page.scss'],
   standalone: true,
-  imports: [IonicModule]
+  imports: [IonicModule,NgFor,RouterModule]
 })
 export class PreferenciaPage{
-
-  constructor(private nav : NavController,private router: Router) { }
-
+  data:any = preferencia;
+  constructor(private nav : NavController,private router: Router) {
+    console.log("preferencia",this.data);
+   }
   regresar(){
     this.nav.back();
   }
